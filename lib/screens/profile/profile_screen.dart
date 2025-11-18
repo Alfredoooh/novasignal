f65c5e207b20220ec1dd62a254ca19c92ce23f18
Alfredoooh <alfredopjonas@gmail.com>
2025-11-18 02:03:12 +0100

    Atualizar o profile_screen.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../../providers/auth_provider.dart';
+import '../../providers/auth_provider.dart'as custom_auth;
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
