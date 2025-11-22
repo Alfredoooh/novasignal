89093433fa84159cabd90543edb640699309a39a
Alfredoooh <alfredopjonas@gmail.com>
2025-11-22 11:30:01 +0100

    Atualizar o message_bubble.dart

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import '../models/message.dart';
 
-class MessageBubble extends StatelessWidget {
+class MessageBubble extends StatefulWidget {
   final Message message;
   final Color primaryColor;
   final Color secondaryBackground;
@@ -14,37 +14,92 @@ class MessageBubble extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final isUser = message.role == 'user';
-    final isError = message.role == 'error';
+  State<MessageBubble> createState() => _MessageBubbleState();
+}
 
-    return Align(
-      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        margin: const EdgeInsets.only(top: 8),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.75,
-        ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 12,
-        ),
-        decoration: BoxDecoration(
-          color: isUser
-              ? primaryColor
-              : isError
-                  ? const Color(0xFF5F1C1C)
-                  : secondaryBackground,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Text(
-          message.content,
-          style: TextStyle(
-            color: isError
-                ? const Color(0xFFFF6B6B)
-                : CupertinoColors.white,
-            fontSize: 16,
-            height: 1.4,
+class _MessageBubbleState extends State<MessageBubble> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isUser = widget.message.role == 'user';
+    final isError = widget.message.role == 'error';
+
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+        child: Align(
+          alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.only(top: 8),
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.75,
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 12,
+            ),
+            decoration: BoxDecoration(
+              color: isUser
+                  ? widget.primaryColor
+                  : isError
+                      ? const Color(0xFF5F1C1C)
+                      : widget.secondaryBackground,
+              borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(20),
+                topRight: const Radius.circular(20),
+                bottomLeft: isUser ? const Radius.circular(20) : const Radius.circular(4),
+                bottomRight: isUser ? const Radius.circular(4) : const Radius.circular(20),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: (isUser ? widget.primaryColor : widget.secondaryBackground).withOpacity(0.3),
+                  blurRadius: 8,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Text(
+              widget.message.content,
+              style: TextStyle(
+                color: isError
+                    ? const Color(0xFFFF6B6B)
+                    : CupertinoColors.white,
+                fontSize: 16,
+                height: 1.4,
+                fontWeight: FontWeight.w400,
+                letterSpacing: -0.2,
+              ),
+            ),
           ),
         ),
       ),
