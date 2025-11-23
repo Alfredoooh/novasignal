70dd473ddcde290ec268c89b920b1f5ad485e0f6
Alfredoooh <alfredopjonas@gmail.com>
2025-11-23 19:54:44 +0100

    Atualizar o home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,256 +1,163 @@
 import 'package:flutter/material.dart';
-import '../widgets/custom_app_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../providers/theme_provider.dart';
+import '../providers/language_provider.dart';
+import '../widgets/settings_modal.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  final ThemeProvider themeProvider;
+  final LanguageProvider languageProvider;
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  const HomeScreen({
+    Key? key,
+    required this.themeProvider,
+    required this.languageProvider,
+  }) : super(key: key);
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedTab = 0;
-
-  final List<String> _tabs = ['Image', 'Video', 'Styles', 'Top Creators'];
-
-  // Lista de imagens com diferentes aspectos para replicar o design
-  final List<ImageItem> _images = [
-    ImageItem(
-      gradient: [Color(0xFF1E40AF), Color(0xFF7C3AED)],
-      aspectRatio: 0.75,
-      likes: 256,
-      hasRecreateButton: true,
-    ),
-    ImageItem(
-      gradient: [Color(0xFFBAE6FD), Color(0xFFE0F2FE)],
-      aspectRatio: 1.3,
-      isLight: true,
-    ),
-    ImageItem(
-      gradient: [Color(0xFF0F766E), Color(0xFF115E59)],
-      aspectRatio: 0.6,
-    ),
-    ImageItem(
-      gradient: [Color(0xFFDC2626), Color(0xFFEA580C)],
-      aspectRatio: 1.0,
-    ),
-    ImageItem(
-      gradient: [Color(0xFFE0F2FE), Color(0xFF93C5FD)],
-      aspectRatio: 1.5,
-      isLight: true,
-    ),
-    ImageItem(
-      gradient: [Color(0xFF0EA5E9), Color(0xFF0284C7)],
-      aspectRatio: 1.3,
-    ),
-    ImageItem(
-      gradient: [Color(0xFF059669), Color(0xFF10B981)],
-      aspectRatio: 1.0,
-    ),
-  ];
+  void _showSettingsModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (context) => SettingsModal(
+        themeProvider: themeProvider,
+        languageProvider: languageProvider,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+
     return Scaffold(
-      appBar: const CustomAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 20, 16, 16),
-            child: Text(
-              'Get inspired by the community',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          _buildTabBar(),
-          const SizedBox(height: 16),
-          Expanded(
-            child: _buildMasonryGrid(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTabBar() {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        children: List.generate(_tabs.length, (index) {
-          final isSelected = _selectedTab == index;
-          return Padding(
-            padding: const EdgeInsets.only(right: 12),
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  _selectedTab = index;
-                });
-              },
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 28,
-                  vertical: 14,
-                ),
-                decoration: BoxDecoration(
-                  color: isSelected ? Colors.white : Colors.transparent,
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: isSelected ? Colors.white : Colors.grey.shade800,
-                    width: 1.5,
+      backgroundColor: theme.scaffoldBackgroundColor,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    languageProvider.translate('home'),
+                    style: theme.textTheme.displayLarge,
                   ),
-                ),
-                child: Text(
-                  _tabs[index],
-                  style: TextStyle(
-                    color: isSelected ? Colors.black : Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ),
-          );
-        }),
-      ),
-    );
-  }
-
-  Widget _buildMasonryGrid() {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                _buildImageCard(_images[0], 0),
-                const SizedBox(height: 12),
-                _buildImageCard(_images[2], 2),
-                const SizedBox(height: 12),
-                _buildImageCard(_images[4], 4),
-                const SizedBox(height: 12),
-                _buildImageCard(_images[6], 6),
-              ],
-            ),
-          ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              children: [
-                _buildImageCard(_images[1], 1),
-                const SizedBox(height: 12),
-                _buildImageCard(_images[3], 3),
-                const SizedBox(height: 12),
-                _buildImageCard(_images[5], 5),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildImageCard(ImageItem item, int index) {
-    final width = (MediaQuery.of(context).size.width - 44) / 2;
-    final height = width / item.aspectRatio;
-
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: item.gradient,
-        ),
-      ),
-      child: Stack(
-        children: [
-          if (item.likes != null)
-            Positioned(
-              top: 12,
-              right: 12,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 6,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(
-                      Icons.favorite_border,
-                      color: Colors.white,
-                      size: 16,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      '${item.likes}',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 13,
-                        fontWeight: FontWeight.w600,
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () => _showSettingsModal(context),
+                      borderRadius: BorderRadius.circular(22),
+                      child: Container(
+                        width: 44,
+                        height: 44,
+                        decoration: BoxDecoration(
+                          color: theme.cardColor,
+                          border: Border.all(color: theme.dividerColor),
+                          borderRadius: BorderRadius.circular(22),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.06),
+                              blurRadius: 8,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: SvgPicture.asset(
+                            'assets/icons/settings.svg',
+                            width: 22,
+                            height: 22,
+                            colorFilter: ColorFilter.mode(
+                              colorScheme.primary,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ),
                       ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(24, 0, 24, 120),
+                child: Column(
+                  children: [
+                    _InfoCard(
+                      title: languageProvider.translate('welcome'),
+                      description: languageProvider.translate('welcomeDesc'),
+                      theme: theme,
+                    ),
+                    const SizedBox(height: 16),
+                    _InfoCard(
+                      title: languageProvider.translate('recent'),
+                      description: languageProvider.translate('recentDesc'),
+                      theme: theme,
+                    ),
+                    const SizedBox(height: 16),
+                    _InfoCard(
+                      title: languageProvider.translate('favorites'),
+                      description: languageProvider.translate('favoritesDesc'),
+                      theme: theme,
                     ),
                   ],
                 ),
               ),
             ),
-          if (item.hasRecreateButton)
-            Positioned(
-              bottom: 16,
-              left: 16,
-              right: 16,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                  elevation: 0,
-                ),
-                child: const Text(
-                  'Recreate',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class ImageItem {
-  final List<Color> gradient;
-  final double aspectRatio;
-  final int? likes;
-  final bool hasRecreateButton;
-  final bool isLight;
+class _InfoCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final ThemeData theme;
 
-  ImageItem({
-    required this.gradient,
-    required this.aspectRatio,
-    this.likes,
-    this.hasRecreateButton = false,
-    this.isLight = false,
+  const _InfoCard({
+    required this.title,
+    required this.description,
+    required this.theme,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: theme.cardColor,
+            border: Border.all(color: theme.dividerColor),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: theme.textTheme.titleMedium,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                description,
+                style: theme.textTheme.bodyMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
