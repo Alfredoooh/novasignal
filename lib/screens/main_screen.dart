4d27d983ff804316cd61ce6c72625b72ba78ccd0
Alfredoooh <alfredopjonas@gmail.com>
2025-11-27 00:01:42 +0100

    Atualizar o main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -22,34 +22,99 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
-
-  late final List<Widget> _screens;
-
-  @override
-  void initState() {
-    super.initState();
-    _screens = [
-      HomeScreen(
-        themeProvider: widget.themeProvider,
-        languageProvider: widget.languageProvider,
-      ),
-      AIScreen(languageProvider: widget.languageProvider),
-      NewScreen(languageProvider: widget.languageProvider),
-    ];
-  }
+  Widget _secondaryScreen = const SizedBox.shrink();
 
   void _onTabChanged(int index) {
     setState(() {
       _currentIndex = index;
+      // Limpa a tela secundária ao trocar de aba
+      _secondaryScreen = const SizedBox.shrink();
     });
+  }
+
+  void _onSecondaryScreenChanged(Widget screen) {
+    setState(() {
+      _secondaryScreen = screen;
+    });
+  }
+
+  bool _isDesktop(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 1024;
   }
 
   @override
   Widget build(BuildContext context) {
+    final isDesktop = _isDesktop(context);
+
+    Widget currentScreen;
+    switch (_currentIndex) {
+      case 0:
+        currentScreen = HomeScreen(
+          themeProvider: widget.themeProvider,
+          languageProvider: widget.languageProvider,
+          onSecondaryScreenChanged: isDesktop ? _onSecondaryScreenChanged : null,
+        );
+        break;
+      case 1:
+        currentScreen = AIScreen(languageProvider: widget.languageProvider);
+        break;
+      case 2:
+        currentScreen = NewScreen(languageProvider: widget.languageProvider);
+        break;
+      default:
+        currentScreen = HomeScreen(
+          themeProvider: widget.themeProvider,
+          languageProvider: widget.languageProvider,
+          onSecondaryScreenChanged: isDesktop ? _onSecondaryScreenChanged : null,
+        );
+    }
+
+    if (isDesktop) {
+      return Scaffold(
+        body: Row(
+          children: [
+            // Sidebar
+            BottomBar(
+              currentIndex: _currentIndex,
+              onTabChanged: _onTabChanged,
+              languageProvider: widget.languageProvider,
+              isDarkMode: widget.themeProvider.isDarkMode,
+            ),
+            // Tela principal
+            Expanded(
+              child: currentScreen,
+            ),
+            // Tela secundária (se houver)
+            if (_secondaryScreen is! SizedBox)
+              Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: Theme.of(context).dividerColor,
+                      width: 1,
+                    ),
+                  ),
+                ),
+                child: _secondaryScreen,
+              ),
+          ],
+        ),
+      );
+    }
+
+    // Layout mobile
     return Scaffold(
       body: IndexedStack(
         index: _currentIndex,
-        children: _screens,
+        children: [
+          HomeScreen(
+            themeProvider: widget.themeProvider,
+            languageProvider: widget.languageProvider,
+          ),
+          AIScreen(languageProvider: widget.languageProvider),
+          NewScreen(languageProvider: widget.languageProvider),
+        ],
       ),
       extendBody: true,
       bottomNavigationBar: BottomBar(
