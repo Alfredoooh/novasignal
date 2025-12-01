f1b4a52ed57ba11a533cf444d8e0088795173730
Alfredoooh <alfredopjonas@gmail.com>
2025-12-01 15:25:21 +0100

    Delete lib directory

--- lib/utils/web_utils.dart
+++
@@ -1,4 +0,0 @@
-// Import condicional - escolhe automaticamente web ou mobile ou stub
-export 'web_utils_stub.dart'
-    if (dart.library.html) 'web_utils_web.dart'
-    if (dart.library.io) 'web_utils_mobile.dart';
