79f9cd28f1749766b5aff36df617040dd40eecf6
Alfredoooh <alfredopjonas@gmail.com>
2025-12-01 22:04:15 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'home_page.dart';
+import 'providers/theme_provider.dart';
 
 void main() {
   runApp(const DocuGenApp());
@@ -10,14 +12,17 @@ class DocuGenApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DocuGen AI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MaterialApp(
+        title: 'DocuGen AI',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        home: const DocuGenHomePage(),
       ),
-      home: const DocuGenHomePage(),
     );
   }
 }
