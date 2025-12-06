404e60fdb5c48dfca7cc248d1deae8f8be73ba01
Alfredoooh <alfredopjonas@gmail.com>
2025-12-07 00:38:28 +0100

    Criar edit_document_screen_web_js_stub.dart

---
+++ lib/screens/edit_document_screen_web_js_stub.dart
@@ -0,0 +1,6 @@
+// lib/screens/edit_document_screen_web_js_stub.dart
+// Stub mínimo para dart:js_util funcionalidades usadas (quando não-web).
+
+dynamic get globalThis => null;
+
+dynamic callMethod(dynamic target, String method, List args) => null;
