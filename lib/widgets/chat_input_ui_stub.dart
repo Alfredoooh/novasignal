3bf54b9c824f3ec2c5f7f735bf5ec4b9ea5274ce
Alfredoooh <alfredopjonas@gmail.com>
2025-12-07 01:23:00 +0100

    Update and rename chat_input_ui_stub.dart to chat_input_web_ui_stub.dart

--- lib/widgets/chat_input_ui_stub.dart
+++
@@ -1,6 +0,0 @@
-// Stub para dart:ui_web (mobile)
-class PlatformViewRegistry {
-  void registerViewFactory(String viewType, dynamic Function(int) factory) {}
-}
-
-final platformViewRegistry = PlatformViewRegistry();
---
+++ lib/widgets/chat_input_web_ui_stub.dart
@@ -0,0 +1,10 @@
+// lib/widgets/chat_input_web_ui_stub.dart
+// Stub para dart:ui_web usado em builds mobile
+
+class PlatformViewRegistry {
+  void registerViewFactory(String viewType, dynamic Function(int) factory) {
+    // Stub vazio - não executa em mobile
+  }
+}
+
+final platformViewRegistry = PlatformViewRegistry();
