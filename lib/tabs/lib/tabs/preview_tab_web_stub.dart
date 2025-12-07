0c1dc9d0b849825151428077d5f43e64b1de4be2
Alfredoooh <alfredopjonas@gmail.com>
2025-12-07 01:41:08 +0100

    Criar lib/tabs/preview_tab_web_stub.dart

---
+++ lib/tabs/lib/tabs/preview_tab_web_stub.dart
@@ -0,0 +1,14 @@
+// lib/tabs/preview_tab_web_stub.dart
+// Stub vazio para compatibilidade quando não está rodando na web
+
+class DivElement {}
+class ScriptElement {}
+class InputElement {}
+class StyleElement {}
+class FileReader {}
+class Document {
+  dynamic querySelector(String selector) => null;
+  dynamic get head => null;
+}
+
+final document = Document();
