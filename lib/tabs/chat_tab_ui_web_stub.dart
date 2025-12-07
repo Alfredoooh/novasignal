11c5354c28e8f73711c198009bfc16361c447ae3
Alfredoooh <alfredopjonas@gmail.com>
2025-12-07 02:06:33 +0100

    Criar chat_tab_ui_web_stub.dart

---
+++ lib/tabs/chat_tab_ui_web_stub.dart
@@ -0,0 +1,8 @@
+// lib/tabs/chat_tab_ui_web_stub.dart
+// Stub vazio para compatibilidade quando não está rodando na web
+
+class PlatformViewRegistry {
+  void registerViewFactory(String viewType, dynamic Function(int) factory) {}
+}
+
+final platformViewRegistry = PlatformViewRegistry();
