80c510be4bdb9a9e5dc65a96a1d3b04c54c3f456
Alfredoooh <alfredopjonas@gmail.com>
2025-12-07 01:09:44 +0100

    Criar chat_input_ui_stub.dart

---
+++ lib/widgets/chat_input_ui_stub.dart
@@ -0,0 +1,6 @@
+// Stub para dart:ui_web (mobile)
+class PlatformViewRegistry {
+  void registerViewFactory(String viewType, dynamic Function(int) factory) {}
+}
+
+final platformViewRegistry = PlatformViewRegistry();
