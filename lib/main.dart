7a15e535934e33a8125062dd7af7d82dcd865e6e
Alfredoooh <alfredopjonas@gmail.com>
2025-12-15 02:23:13 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class MyApp extends StatelessWidget {
                     systemOverlayStyle: SystemUiOverlayStyle.light,
                   ),
                 ),
-                home: HomePage(),
+                home: const HomePage(),
                 debugShowCheckedModeBanner: false,
               );
             },
