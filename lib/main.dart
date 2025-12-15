db226f6339b0336691efc615a41a9be2b845bdcf
Alfredoooh <alfredopjonas@gmail.com>
2025-12-15 01:37:04 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           theme: _buildLightTheme(),
           darkTheme: _buildDarkTheme(),
           themeMode: appState.temaEscuro ? ThemeMode.dark : ThemeMode.light,
-          home: HomePage(),
+          home: const HomePage(),
         );
       },
     );
