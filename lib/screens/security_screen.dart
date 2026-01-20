870731d80cd8841256292057629660b556cd036c
Alfredoooh <alfredopjonas@gmail.com>
2026-01-20 23:10:07 +0100

    Criar security_screen.dart

---
+++ lib/screens/security_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class SecurityScreen extends StatelessWidget {
+  const SecurityScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Container();
+}
