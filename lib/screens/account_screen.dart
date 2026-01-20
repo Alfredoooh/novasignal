5528bfe0c818d9614be8360d411f3738b288e193
Alfredoooh <alfredopjonas@gmail.com>
2026-01-20 23:10:53 +0100

    Criar account_screen.dart

---
+++ lib/screens/account_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Container();
+}
