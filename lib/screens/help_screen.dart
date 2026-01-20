aa98713dfd577b320a16a7302249d4771369f85d
Alfredoooh <alfredopjonas@gmail.com>
2026-01-20 23:09:14 +0100

    Criar help_screen.dart

---
+++ lib/screens/help_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class HelpScreen extends StatelessWidget {
+  const HelpScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Container();
+}
