aef73a27c603a60f249b641b59fd6132d3973c08
Alfredoooh <alfredopjonas@gmail.com>
2026-01-21 13:28:50 +0100

    Atualizar o cart_provider.dart

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-
 class CartProvider extends InheritedWidget {
   final List<Map<String, dynamic>> cart;
   final Function(Map<String, dynamic>) addToCart;
   final Function(Map<String, dynamic>) removeFromCart;
+  final Function(Map<String, dynamic>, int)? updateQuantity; // Adicionar isto
 
   const CartProvider({
     Key? key,
     required this.cart,
     required this.addToCart,
     required this.removeFromCart,
+    this.updateQuantity,
     required Widget child,
   }) : super(key: key, child: child);
 
