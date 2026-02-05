b2bbb0598ecd4d3b0997ddf762cedcc968c9f2ed
Alfredoooh <alfredopjonas@gmail.com>
2026-02-05 13:58:47 +0100

    Delete lib directory

--- lib/screens/help_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
-
-class HelpScreen extends StatelessWidget {
-  const HelpScreen({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) => Container();
-}
--- lib/screens/security_screen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:material_symbols_icons/material_symbols_icons.dart';
-
-class SecurityScreen extends StatelessWidget {
-  const SecurityScreen({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) => Container();
-}
