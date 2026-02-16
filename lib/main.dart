6f4efc2944d02f0208d907e21d474f1132ec55fa
Alfredoooh <alfredopjonas@gmail.com>
2026-02-16 11:40:11 +0100

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,158 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:provider/provider.dart';
+import 'providers/balance_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      systemNavigationBarColor: Colors.transparent,
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BalanceProvider()),
+      ],
+      child: CoinBoxApp(),
     ),
   );
-  
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  
-  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class CoinBoxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Testing App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+      title: 'Coin Box',
       debugShowCheckedModeBanner: false,
-      home: const WebViewPage(),
-    );
-  }
-}
-
-class WebViewPage extends StatefulWidget {
-  const WebViewPage({Key? key}) : super(key: key);
-
-  @override
-  State<WebViewPage> createState() => _WebViewPageState();
-}
-
-class _WebViewPageState extends State<WebViewPage> with WidgetsBindingObserver {
-  final GlobalKey webViewKey = GlobalKey();
-  InAppWebViewController? webViewController;
-  double progress = 0;
-  String url = "https://testing-ddy8.onrender.com/";
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    _updateSystemUI();
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangePlatformBrightness() {
-    _updateSystemUI();
-  }
-
-  void _updateSystemUI() {
-    final brightness = WidgetsBinding.instance.platformDispatcher.platformBrightness;
-    final isDark = brightness == Brightness.dark;
-    
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
-        statusBarBrightness: isDark ? Brightness.dark : Brightness.light,
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
+      theme: ThemeData(
+        primaryColor: Color(0xFF0088CC),
+        scaffoldBackgroundColor: Colors.white,
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (await webViewController?.canGoBack() ?? false) {
-          webViewController?.goBack();
-          return false;
-        }
-        return true;
-      },
-      child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).padding.top,
-              color: Colors.transparent,
-            ),
-            Expanded(
-              child: SafeArea(
-                top: false,
-                child: InAppWebView(
-                  key: webViewKey,
-                  initialUrlRequest: URLRequest(url: WebUri(url)),
-                  initialSettings: InAppWebViewSettings(
-                    javaScriptEnabled: true,
-                    javaScriptCanOpenWindowsAutomatically: true,
-                    useOnDownloadStart: true,
-                    useShouldOverrideUrlLoading: true,
-                    mediaPlaybackRequiresUserGesture: false,
-                    allowFileAccessFromFileURLs: true,
-                    allowUniversalAccessFromFileURLs: true,
-                    cacheEnabled: true,
-                    domStorageEnabled: true,
-                    databaseEnabled: true,
-                    useHybridComposition: true,
-                    allowContentAccess: true,
-                    allowFileAccess: true,
-                    supportMultipleWindows: true,
-                    allowsInlineMediaPlayback: true,
-                    allowsBackForwardNavigationGestures: true,
-                    supportZoom: false,
-                    builtInZoomControls: false,
-                    displayZoomControls: false,
-                  ),
-                  onWebViewCreated: (controller) {
-                    webViewController = controller;
-                  },
-                  onLoadStart: (controller, url) {
-                    setState(() {
-                      this.url = url.toString();
-                    });
-                  },
-                  onLoadStop: (controller, url) async {
-                    setState(() {
-                      this.url = url.toString();
-                    });
-                  },
-                  onProgressChanged: (controller, progress) {
-                    setState(() {
-                      this.progress = progress / 100;
-                    });
-                  },
-                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                    setState(() {
-                      this.url = url.toString();
-                    });
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
+      darkTheme: ThemeData(
+        primaryColor: Color(0xFF0088CC),
+        scaffoldBackgroundColor: Color(0xFF18191A),
+        brightness: Brightness.dark,
       ),
+      home: HomeScreen(),
     );
   }
 }
