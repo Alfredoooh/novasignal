59a45b4a9c9b3c50019e4dfe3f679ab9e0d61ffe
Alfredoooh <alfredopjonas@gmail.com>
2026-02-27 13:43:22 +0100

    Atualizar o auth_screen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -11,6 +11,8 @@ const _svgLock = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 const _svgEyeOn = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.271,9.419C21.72,6.893,18.192,2.655,12,2.655S2.28,6.893.729,9.419a4.908,4.908,0,0,0,0,5.162C2.28,17.107,5.808,21.345,12,21.345s9.72-4.238,11.271-6.764A4.908,4.908,0,0,0,23.271,9.419Zm-1.705,4.115C20.234,15.7,17.219,19.345,12,19.345S3.766,15.7,2.434,13.534a2.918,2.918,0,0,1,0-3.068C3.766,8.3,6.781,4.655,12,4.655s8.234,3.641,9.566,5.811A2.918,2.918,0,0,1,21.566,13.534Z"/><path d="M12,7a5,5,0,1,0,5,5A5.006,5.006,0,0,0,12,7Zm0,8a3,3,0,1,1,3-3A3,3,0,0,1,12,15Z"/></svg>''';
 const _svgEyeOff = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.271,9.419A15.866,15.866,0,0,0,19.9,5.51l2.8-2.8a1,1,0,0,0-1.414-1.414L18.241,4.345A12.054,12.054,0,0,0,12,2.655C5.809,2.655,2.281,6.893.729,9.419a4.908,4.908,0,0,0,0,5.162A15.866,15.866,0,0,0,4.1,18.49l-2.8,2.8a1,1,0,1,0,1.414,1.414l3.052-3.052A12.054,12.054,0,0,0,12,21.345c6.191,0,9.719-4.238,11.271-6.764A4.908,4.908,0,0,0,23.271,9.419ZM2.433,13.534a2.918,2.918,0,0,1,0-3.068C3.767,8.3,6.782,4.655,12,4.655A10.1,10.1,0,0,1,16.766,5.82L14.753,7.833a4.992,4.992,0,0,0-6.92,6.92l-2.31,2.31A13.723,13.723,0,0,1,2.433,13.534ZM15,12a3,3,0,0,1-3,3,2.951,2.951,0,0,1-1.285-.3L14.7,10.715A2.951,2.951,0,0,1,15,12ZM9,12a3,3,0,0,1,3-3,2.951,2.951,0,0,1,1.285.3L9.3,13.285A2.951,2.951,0,0,1,9,12Zm12.567,1.534C20.233,15.7,17.218,19.345,12,19.345A10.1,10.1,0,0,1,7.234,18.18l2.013-2.013a4.992,4.992,0,0,0,6.92-6.92l2.31-2.31a13.723,13.723,0,0,1,3.09,3.529A2.918,2.918,0,0,1,21.567,13.534Z"/></svg>''';
 
+const _svgChevronFwd = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"/></svg>''';
+
 Widget _svgIcon(String data, Color color, {double size = 18}) => SvgPicture.string(
   data, width: size, height: size,
   colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
@@ -81,16 +83,7 @@ class AuthScreen extends StatelessWidget {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    // Label acima dos botões
-                    Text(
-                      'Comece com um login',
-                      style: GoogleFonts.roboto(
-                        color: Colors.white.withOpacity(0.75),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
+
                     // Botão Login
                     _MainButton(
                       label: 'Fazer login com telemóvel ou email',
@@ -105,6 +98,37 @@ class AuthScreen extends StatelessWidget {
                       bg: _btnRegBg,
                       textColor: _btnRegText,
                       onTap: () => _openRegister(context),
+                    ),
+                    const SizedBox(height: 20),
+                    // Iniciar sem login
+                    GestureDetector(
+                      onTap: onDone,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Iniciar sem login',
+                            style: GoogleFonts.roboto(
+                              color: Colors.white.withOpacity(0.28),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          // chevron-forward ionicon via unicode (Ionicons usa Material-like fallback)
+                          // Usamos HtmlElementView ou widget de texto com o caracter do ionicon
+                          SvgPicture.string(
+                            _svgChevronFwd,
+                            width: 16,
+                            height: 16,
+                            colorFilter: ColorFilter.mode(
+                              Colors.white.withOpacity(0.28),
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -200,7 +224,7 @@ class _LoginModalState extends State<_LoginModal> {
       child: Container(
         decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+          borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
         ),
         padding: const EdgeInsets.fromLTRB(24, 20, 24, 36),
         child: Form(
@@ -340,7 +364,7 @@ class _RegisterModalState extends State<_RegisterModal> {
         height: screenH * 0.9,
         decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+          borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
         ),
         padding: const EdgeInsets.fromLTRB(24, 20, 24, 36),
         child: Form(
